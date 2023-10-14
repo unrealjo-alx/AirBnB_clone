660dfa6112ecbf72403bc39020831a324a2ae6bf
Youssef Eddaif <jo9unreal@gmail.com>
2023-10-14 09:46:22 +0100

    Create BaseModel from dictionary.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,21 @@ class BaseModel:
     Base model with common attributes and methods.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "updated_at" or key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
