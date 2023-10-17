a355830b0df2d7a13f18ebcdee01a1114beacdbc
Youssef Eddaif <jo9unreal@gmail.com>
2023-10-17 12:52:22 +0100

    6. Console 0.0.1.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+console.py : The main program entry point.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Exit the program on EOF (Ctrl+D)
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered
+        """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
