b4823e15a9d095ceb582b34a51ee7abbdcc2d8bf
Youssef Eddaif <jo9unreal@gmail.com>
2023-10-17 12:42:52 +0100

    Update unit tests for BaseModel.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,43 +1,34 @@
-"""
-Unit tests for the BaseModel class.
-"""
-
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test cases for the BaseModel class."""
-
     def test_init(self):
-        """Test the initialization of a BaseModel object."""
-        a_model = BaseModel()
-        self.assertIsInstance(a_model, BaseModel)
-        self.assertEqual(len(a_model.id), 36)
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
+        self.assertEqual(len(model.id), 36)
 
     def test_save(self):
-        """Test the save method of a BaseModel object."""
-        b_model = BaseModel()
-        initial_updated_at = b_model.updated_at
-        b_model.save()
-        self.assertNotEqual(b_model.updated_at, initial_updated_at)
-        self.assertGreater(b_model.updated_at, initial_updated_at)
+        model = BaseModel()
+        initial_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(model.updated_at, initial_updated_at)
+        self.assertGreater(model.updated_at, initial_updated_at)
 
     def test_to_dict(self):
-        """Test the to_dict method of a BaseModel object."""
-        c_model = BaseModel()
-        d_model = BaseModel(**c_model.to_dict())
-        self.assertEqual(c_model.id, d_model.id)
-        self.assertEqual(c_model.created_at, d_model.created_at)
-        self.assertEqual(c_model.updated_at, d_model.updated_at)
-        self.assertEqual(c_model.to_dict(), d_model.to_dict())
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertEqual(model_dict["id"], model.id)
+        self.assertEqual(model_dict["created_at"], model.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"], model.updated_at.isoformat())
 
     def test_str(self):
-        """Test the __str__ method of a BaseModel object."""
-        e_model = BaseModel()
-        string_representation = f"[BaseModel] ({e_model.id}) {e_model.__dict__}"
-        self.assertEqual(str(e_model), string_representation)
+        model = BaseModel()
+        expected_string = f"[BaseModel] ({model.id}) {model.__dict__}"
+        self.assertEqual(str(model), expected_string)
 
 
 if __name__ == "__main__":
